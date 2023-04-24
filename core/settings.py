9758675c70b85fd9e67aaffa2c5226d6c6633fc0
Stephen Chesnowitz <schesnowitz@users.noreply.github.com>
2023-04-24 00:53:26 -0400

    rwdb

--- core/settings.py
+++ core/settings.py
@@ -69,28 +69,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": env('PGDATABASE'), 
-#         "USER": env('PGUSER'),
-#         "PASSWORD": env('PGPASSWORD'),
-#         "HOST": env('PGHOST'),
-#         "PORT": env('PGPORT'),
-#     }
-# }
-import dj_database_url
-
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env('PGDATABASE'), 
+        "USER": env('PGUSER'),
+        "PASSWORD": env('PGPASSWORD'),
+        "HOST": env('PGHOST'),
+        "PORT": env('PGPORT'),
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
+
 
 
 
