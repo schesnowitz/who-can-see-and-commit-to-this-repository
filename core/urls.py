ebf28c676ed3e9dd5c6478a566af72049e7a2c50
Stephen Chesnowitz <schesnowitz@users.noreply.github.com>
2023-05-09 01:15:13 -0400

    +=static

--- core/urls.py
+++ core/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     # path("success/", successView, name="success"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
