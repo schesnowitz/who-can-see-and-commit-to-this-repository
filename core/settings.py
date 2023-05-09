d8801255e7633438663c97d6d0323cfe35c480ab
Stephen Chesnowitz <schesnowitz@users.noreply.github.com>
2023-05-09 15:16:55 -0400

    STATICFILES_DIRS

--- core/settings.py
+++ core/settings.py
@@ -109,7 +109,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles" 
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
