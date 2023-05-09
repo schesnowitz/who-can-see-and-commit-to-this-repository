3687ce461a16309b6ad62e3d2921ed4f0d9ca258
Stephen Chesnowitz <schesnowitz@users.noreply.github.com>
2023-05-09 17:20:01 -0400

    parrot

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles" 
 
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # python manage.py collectstatic
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
